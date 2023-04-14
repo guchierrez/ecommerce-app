fb5d3b5f1e65bb5a214d7ee8c83c556b791ec31d
unknown <marcelinogs2001@gmail.com>
2023-04-14 17:34:11 +0900

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: "src/assets", dest: "dist" },
+        { src: "products.json", dest: "dist" },
+      ],
+    }),
+  ],
   base: "/ecommerce-app/",
 });
