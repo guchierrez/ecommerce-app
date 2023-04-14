815633a58657af7406820248a344279717987a77
unknown <marcelinogs2001@gmail.com>
2023-04-14 17:30:20 +0900

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import copy from "fs-extra/copy";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/ecommerce-app/",
-  build: {
-    rollupOptions: {
-      input: "index.html",
-    },
-    outDir: "dist",
-    assetsDir: "assets",
-    minify: "terser",
-    emptyOutDir: true,
-    write: true,
-    afterBuild: async () => {
-      await copy("src/assets", "dist/assets");
-      await copy("products.json", "dist/products.json");
-    },
-  },
 });
